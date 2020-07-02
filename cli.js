ace4a49357d13549441cc82ea1a7e5c331b52a4e
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 11:28:10 -0300

    Correção e transposição de todas as funções para o cli.js a fim de ver se o problema com o download direto do github para de apresentar o problema de encontrar as referências

--- cli.js
+++ cli.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
-const checkLinks = require('./checkhttp.js')
+const chalk = require('chalk');
+const https = require('https');
+//const checkLinks = require('./checkhttp.js')
 
 const mdLinks = require('./index.js');
 
 const file = process.argv[2];
 
+function checkLinks(obj) {
+  https.get(obj.href, (res) => {
+    console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
+  })
+    .on('error', (e) => console.error(e.code));
+}
+
 mdLinks(file)
   .then((result) => {
     result.forEach((item) => {
-      console.log(item)
+      checkLinks(item)
     });
   })
   .catch((error) => console.log(error));
