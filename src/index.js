9181504a260f4e16e1d71eb7f758735e8e8192ad
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 09:17:07 -0300

    Função mdLinks criada e funcionando

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-import {lala} from "./cli.js"
-console.log(lala())
+const fs = require('fs');
+
+let arr = [];
+
+function mdLinks(file) {
+  //console.log(file)
+  return new Promise((resolved, rejected) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        rejected(err.message);
+      } else {
+
+        const regex = data.match(/\[(.*?)\][\s]?\(((http[s]?:\/\/|www\.)\w.*)\)/gm);
+        //console.log(regex)
+        regex.forEach((i) => {
+          arr.push({
+            text: i.match(/\[(.*?)\][\s]?/)[1],
+            href: i.match(/[\s]?\(((http[s]?:\/\/|www\.)\w.*)\)/)[1],
+            fileName: file
+          });
+        });
+        resolved(arr);
+        arr = []
+      }
+    });
+  });
+}
+
+module.exports = mdLinks;
