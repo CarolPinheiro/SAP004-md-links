b9ded8b7ace7bccc057726e5fa7f84bbc89c0992
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 13:11:58 -0300

    Função checkhttp atualizada para receber o parâmetro validate

--- checkhttp.js
+++ checkhttp.js
@@ -1,12 +1,18 @@
-#!/usr/bin/env node
+
 const chalk = require('chalk');
 const https = require('https');
 
-function checkLinks(obj) {
-  https.get(obj.href, (res) => {
-    console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
-  })
-    .on('error', (e) => console.error(e.code));
+function checkLinks(obj, validation = false) {
+  if (validation === '--validate'){
+      https.get(obj.href, (res) => {
+          console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
+      })
+          .on('error', (e) => console.error(e.code));
+  } else {
+      console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)} Path:${obj.fileName}`);
+
+  }
+
 }
 
 module.exports = checkLinks;
