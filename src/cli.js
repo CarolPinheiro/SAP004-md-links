753374bdcd69013ae1070b3997b03d7f841e91cf
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 09:21:29 -0300

    Resolução da promise retornada para a função check-links, o arquivo cli apenas se ocupa de solucionar a promise

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,15 @@
-export const lala = () => console.log("deu certo")
+#!/usr/bin/env node
+
+const checkLinks = require('../checkhttp.js')
+
+const mdLinks = require('./index.js');
+
+const file = process.argv[2];
+
+mdLinks(file)
+  .then((result) => {
+    result.forEach((item) => {
+      checkLinks(item);
+    });
+  })
+  .catch((error) => console.log(error));
