8fb22c1e62c338c1d49b94b78c7a74215bc13ae1
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 16:45:24 -0300

    checkhttp identado para 2 espaços

--- checkhttp.js
+++ checkhttp.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 const https = require('https');
 
 function checkLinks(obj, validation = false) {
-  if (validation === '--validate'){
-      https.get(obj.href, (res) => {
-          console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
-      })
-          .on('error', (e) => console.error(e.code));
+  if (validation === '--validate') {
+    https.get(obj.href, (res) => {
+      console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
+    })
+      .on('error', (e) => console.error(e.code));
   } else {
-      console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)} Path:${obj.fileName}`);
+    console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)} Path:${obj.fileName}`);
 
   }
 
