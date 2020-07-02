3a5841716847cf20976f9c6dadcb45fa32ecfe91
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-02 09:20:43 -0300

    Função checkLinks corrigida com as bibliotecas necessárias sendo importadas

--- checkhttp.js
+++ checkhttp.js
@@ -1,13 +1,11 @@
-function checkLinks(obj, httpValidate = false) {
-    if (httpValidate.validation) {
-        https.get(obj.href, (res) => {
-console.log(res.statusCode)
-        })
-            .on('error', (e) => console.error(e.code));
-    } else {
+const chalk = require('chalk');
+const https = require('https');
 
-    }
-
+function checkLinks(obj) {
+  https.get(obj.href, (res) => {
+    console.log(`Link: ${chalk.blueBright.bold(obj.href)} Domínio: ${chalk.cyanBright.bold(obj.text)}, httpStatus:${chalk.green.bold(res.statusCode)}, Message:${chalk.yellow.bold(res.statusMessage)}`);
+  })
+    .on('error', (e) => console.error(e.code));
 }
 
 module.exports = checkLinks;
