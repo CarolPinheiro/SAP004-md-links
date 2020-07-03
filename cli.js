c3e8d3e07741f3971609ede4176651af65788176
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-03 13:09:55 -0300

    Filtro com regex adicionado para que apenas os elementos corretos sejam pegos

--- cli.js
+++ cli.js
@@ -8,20 +8,21 @@ const readPath = require('./readdirectories.js');
 const path = require('path');
 
 function checkFilesUser(file, validation, stats) {
+    console.log()
     if (/\.md/.test(file)) {
         mdLinks(file)
             .then((result) => {
-                result.forEach((item) => {
-                    checkLinks(item, validation);
-                });
+                const filteredResult = result.filter( item =>/(http[s]?|www)/.test(item.href) )
+                    checkLinks(filteredResult, validation, stats);
             })
             .catch((error) => console.log(error));
     } else {
-        readPath(file, validation)
+        readPath(file, validation, stats)
     }
 }
 
 const file = process.argv[2]
 const filePath = path.resolve(file)
 const validation = process.argv[3]
-checkFilesUser(filePath, validation)
+const stats = process.argv[4]
+checkFilesUser(filePath, validation, stats)
