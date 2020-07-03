948961e239bcf4d2ad4e96ee5e6a19e9fdd372a8
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-03 13:19:58 -0300

    Agora há um filtro para garantir que apenas links entrem para a checkLinks quando for chamada na ReadPath

--- readdirectories.js
+++ readdirectories.js
@@ -1,13 +1,17 @@
 const readdir = require('readdir-recursive');
 const checkLinks = require('./checkhttp.js')
+const mdLinks = require('./index.js')
 
-function readPath(readPath, validation) {
+function readPath(readPath, validation, stats) {
     readdir.file(readPath, (file) => {
         if (/\.md/g.test(file) && !/node_modules/g.test(file)) {
+            
             mdLinks(file)
                 .then((result) => {
-                    result.forEach((obj) => {
-                        checkLinks(obj, validation)
+                    const filteredResult = result.filter( item =>/(http[s]?|www)/.test(item.href) )
+                    filteredResult.forEach((obj) => {
+
+                        checkLinks(obj, validation, stats)
                     })
                 })
 
