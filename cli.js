fcf94e06ba44dbd7071093ff221bd711075ce41f
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 15:27:56 -0300

    Agora a manipulação dos dados para o console do stats é feito em uma função a parte

--- cli.js
+++ cli.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 const mdLinks = require('./index.js');
 const chalk = require('chalk');
-
-const path = require('path');
+const statsReturn = require('./stats')
 
 
 function checkUsersNeed(file, validadeOrStats) {
@@ -10,26 +9,22 @@ function checkUsersNeed(file, validadeOrStats) {
   mdLinks(file, validadeOrStats)
     .then(res => {
       const flatResult = res.flat()
+      const statsReturned =  statsReturn(flatResult)
       if (validadeOrStats.includes('--stats') && validadeOrStats.includes('--validate')) {
-        const allLinks = flatResult.map(i => i.href)
-        const brokenLinks = flatResult.filter(i => i.statusCode > 400 && i.statusCode < 500)
-        const notRepeatedLinks = new Set(allLinks)
-        console.log(`All:${chalk.yellow.bold(allLinks.length)} \nUnique:${chalk.green.bold(notRepeatedLinks.size)}\nBroken:${chalk.red.bold(brokenLinks.length)}`)
+        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}\nBroken:${chalk.red.bold(statsReturned.brokenLinks)}`)
 
       } else if (validadeOrStats.includes('--stats')) {
-        const allLinks = flatResult.map(i => i.href)
-        const notRepeatedLinks = new Set(allLinks)
-        console.log(`All:${chalk.yellow.bold(allLinks.length)} \nUnique:${chalk.green.bold(notRepeatedLinks.size)}`)
+        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}`)
 
       } else {
         flatResult.map(e => {
           if (e.statusCode) {
             console.log(`\n 
-                Text:${chalk.cyan.bold(e.text)} 
-                Href:${chalk.magenta.bold(e.href)} 
-                Path:${chalk.blue.bold(e.file)}
-                StatusCode:${e.statusCode > 400 && e.statusCode < 500 ? chalk.red.bold(e.statusCode) : chalk.green.bold(e.statusCode)}
-                Status Message:${chalk.white.bold(e.message)} \n`)
+            Text:${chalk.cyan.bold(e.text)} 
+            Href:${chalk.magenta.bold(e.href)} 
+            Path:${chalk.blue.bold(e.file)}
+            StatusCode:${e.statusCode > 400 && e.statusCode < 500 ? chalk.red.bold(e.statusCode) : chalk.green.bold(e.statusCode)}
+            Status Message:${chalk.white.bold(e.message)} \n`)
           }
           else {
             console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
@@ -43,3 +38,5 @@ const file = process.argv[2];
 const validadeOrStats = process.argv
 
 checkUsersNeed(file, validadeOrStats)
+
+
