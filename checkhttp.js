423922a4dc36cad08ebf4db100151a65dde6ddda
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 12:26:11 -0300

    Atualização das funções da checkhttp  e exclusão da checktest

--- checkhttp.js
+++ checkhttp.js
@@ -1,47 +1,18 @@
+const https = require('https')
 
-const chalk = require('chalk');
-const https = require('https');
+function checkIfALinkExist(resultado, file) {
+  return new Promise((resolved) => {
 
-function checkLinks(filteredResult, validation, stats) {
-  const arrError = []
-  if (validation === "--stats" && stats === "--validate") {
-    const arrWithLinks = filteredResult.map((item) => item.href)
-    const setArr = new Set(arrWithLinks)
-    filteredResult.map(obj => {
-      if (/https/.test(obj.href)) {
-        https.get(obj.href, (res) => {
-          if (res.statusCode >= 400 && res.statusCode < 500) {
-            arrError.push(res.statusCode)
-            console.log(`All:${filteredResult.length} \n Unique: ${setArr.size} \n Broken: ${arrError.length} `)
-          }
-        })
-          .on('error', (e) => console.log(e))
-      }
+    https.get(resultado[2], (res) => {
+      resolved({ text: resultado[1], href: resultado[2], path: file, validation: res.statusCode, message: res.statusMessage })
     })
-  }
-  else if (validation === '--stats') {
-    const arrWithLinks = filteredResult.map((item) => item.href)
-    const setArr = new Set(arrWithLinks)
-    console.log(`\n Unique: ${arrWithLinks.length} \n All:${setArr.size} `)
-  } else if (validation === '--validate') {
-    filteredResult.map(obj => {
-      if (/https/.test(obj.href)) {
-        https.get(obj.href, (res) => {
-          console.log(`\n Link: ${chalk.blueBright.bold(obj.href)} \n Domínio: ${chalk.cyanBright.bold(obj.text)} \n httpStatus:${chalk.green.bold(res.statusCode)} \n Message:${chalk.yellow.bold(res.statusMessage)}`);
-        })
-          .on('error', (e) => {
-            console.log(`\n Link: ${chalk.blueBright.bold(obj.href)} \n Domínio: ${chalk.cyanBright.bold(obj.text)} \n httpStatus:${chalk.green.bold(404)} \n Message:${chalk.yellow.bold(e)}`);
+      .on('error', (e) => {
+        resolved({ text: resultado[1], href: resultado[2], path: file, validation: 404, message: e.message })
+      })
 
-          });
-      }
-    })
-
-  }
-  else {
-    filteredResult.map(obj => {
-      console.log(`\n Link: ${chalk.blueBright.bold(obj.href)} \n Domínio: ${chalk.cyanBright.bold(obj.text)} \n Path:${obj.file}`);
-    })
-  }
+  })
 }
 
-module.exports = checkLinks;
+
+module.exports = checkIfALinkExist;
+
