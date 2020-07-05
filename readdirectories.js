1c97dc551fb270f613cb43069a2956e72d4fe091
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 13:28:01 -0300

    ReadPath refatorado, para processar o resultado na Readfile e retornar uma promise após isso

--- readdirectories.js
+++ readdirectories.js
@@ -1,10 +1,14 @@
 const readdir = require('readdir-recursive');
+const readFile = require('./readFileAndExtractData');
 
-function readPath(readPath, func) {
-    readdir.file(readPath, (file) => {
-        if (/\.md/g.test(file) && !/node_modules/g.test(file)) {
-            func(file)
-        }
+
+function readPath(path, options) {
+  return new Promise((resolved) => {
+    readdir.file(path, (file) => {
+      if (/\.md/g.test(file) && !/node_modules/g.test(file)) {
+        resolved(readFile(file, options))
+      }
     })
+  })
 }
 module.exports = readPath;
