dc4b46a23216d5246d31df177e228a3c60a41ee1
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 20:06:13 -0300

    readFileAndExtractData corrigido conforme as normas do airbnb

--- src/readFileAndExtractData.js
+++ src/readFileAndExtractData.js
@@ -1,24 +1,28 @@
-const checkIfALinkExist = require('./checkhttp');
 const fs = require('fs');
+const checkIfALinkExist = require('./checkhttp');
 
 function readFile(file, options) {
-    return new Promise((resolved, rejected) => {
-        fs.readFile(file, 'utf-8', (err, data) => {
-            if (err) {
-                rejected(err.message);
-            }
-            const regex = data.match(/\[([^\]]*)\]\((https[^)]*)/gm);
-            const validate = regex.map(i => {
-                const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-                return checkIfALinkExist(result, file)
-            })
-            const textResult = regex.map((i) => {
-                const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-                return { text: result[1], href: result[2], file }
-            });
-            options.includes('--validate') ? resolved(Promise.all(validate)) : resolved(Promise.resolve(textResult))
-        })
-    })
+  return new Promise((resolved, rejected) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        rejected(err.message);
+      }
+      const regex = data.match(/\[([^\]]*)\]\((https[^)]*)/gm);
+      const validate = regex.map((i) => {
+        const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+        return checkIfALinkExist(result, file);
+      });
+      const textResult = regex.map((i) => {
+        const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+        return { text: result[1], href: result[2], file };
+      });
+      if (options.includes('--validate')) {
+        resolved(Promise.all(validate));
+      } else {
+        resolved(Promise.resolve(textResult));
+      }
+    });
+  });
 }
 
 module.exports = readFile;
