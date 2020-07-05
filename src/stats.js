5aee6a78890f216311a18769f056e25ff4a8bb60
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 20:04:53 -0300

    stats corrigido conforme as normas do airbnb

--- src/stats.js
+++ src/stats.js
@@ -1,8 +1,11 @@
 function statsReturn(arr) {
-  const allLinks = arr.map(i => i.href)
-  const brokenLinks = arr.filter(i => i.statusCode > 400 && i.statusCode < 500)
-  const notRepeatedLinks = new Set(allLinks)
-
-  return { allLinks: allLinks.length, brokenLinks: brokenLinks.length, notRepeatedLinks: notRepeatedLinks.size }
+  const allLinks = arr.map((i) => i.href);
+  const brokenLinks = arr.filter((i) => i.statusCode > 400 && i.statusCode < 500);
+  const notRepeatedLinks = new Set(allLinks);
+  return {
+    allLinks: allLinks.length,
+    brokenLinks: brokenLinks.length,
+    notRepeatedLinks: notRepeatedLinks.size,
+  };
 }
 module.exports = statsReturn;
