e3701d68e3732f1504f969f0a6430b41b97edef7
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 16:31:23 -0300

    Identação alterada para 2 espaços

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,75 +1,76 @@
 const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
-    test('mdLinks should be a function', (done) => {
-        expect(typeof mdLinks).toBe('function');
+  test('mdLinks should be a function', (done) => {
+    expect(typeof mdLinks).toBe('function');
+    done()
+  })
+
+  test('should return an array with the objects well defined', (done) => {
+    mdLinks('./test/text.md')
+      .then(result => {
+        expect(result).toEqual([
+          { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: './test/text.md' },
+          { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: './test/text.md' }
+        ])
+        done();
+      })
+  })
+  test('should return the objects with their validation', (done) => {
+    mdLinks('./test/text.md', '--validate')
+      .then(result => {
+        expect(result).toEqual([
+          { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: './test/text.md', message: "OK", statusCode: 200, },
+          { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: './test/text.md', message: "Moved Permanently", statusCode: 301 }
+        ])
+        done();
+      })
+  })
+  test('should return the objects of all the files in the directory with their validation', (done) => {
+    mdLinks('./test/', '--validate')
+      .then(result => {
+        expect(result).toEqual([
+          { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: 'test\\text.md', message: "OK", statusCode: 200, },
+          { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: 'test\\text.md', message: "Moved Permanently", statusCode: 301 }
+        ])
+        done();
+      })
+  })
+
+  test('should return the object with all the files in the directory', (done) => {
+    mdLinks('./test/')
+      .then(result => {
+        expect(result).toEqual([[{
+          "file": "test\\text.md",
+          "href": "https://nodejs.org/pt-br/",
+          "text": "Node js"
+        },
+        {
+          "file": "test\\text.md",
+          "href": "https://developers.google.com/v8/",
+          "text": "motor de JavaScript V8 do Chrome"
+        }],
+        [{
+          "file": "test\\text.md",
+          "href": "https://nodejs.org/pt-br/",
+          "text": "Node js"
+        }, {
+          "file": "test\\text.md",
+          "href": "https://developers.google.com/v8/",
+          "text": "motor de JavaScript V8 do Chrome"
+        }]]
+        )
+        done();
+      })
+  })
+
+
+
+  test('erro', (done) => {
+    mdLinks('./tests/text.md')
+      .catch(err => {
+        expect(err).toBe("Sorry, but there's no archive with that name in this directory");
         done()
-    })
-
-    test('should return an array with the objects well defined', (done) => {
-        mdLinks('./test/text.md')
-            .then(result => {
-                expect(result).toEqual([
-                    { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: './test/text.md' },
-                    { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: './test/text.md' }
-                ])
-                done();
-            })
-    })
-    test('should return the objects with their validation', (done) => {
-        mdLinks('./test/text.md', '--validate')
-            .then(result => {
-                expect(result).toEqual([
-                    { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: './test/text.md', message: "OK", statusCode: 200, },
-                    { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: './test/text.md', message: "Moved Permanently", statusCode: 301 }
-                ])
-                done();
-            })
-    })
-    test('should return the objects of all the files in the directory with their validation', (done) => {
-        mdLinks('./test/', '--validate')
-            .then(result => {
-                expect(result).toEqual([
-                    { href: 'https://nodejs.org/pt-br/', text: 'Node js', file: 'test\\text.md', message: "OK", statusCode: 200, },
-                    { href: 'https://developers.google.com/v8/', text: 'motor de JavaScript V8 do Chrome', file: 'test\\text.md', message: "Moved Permanently", statusCode: 301 }
-                ])
-                done();
-            })
-    })
-    test('should return the objects of all the files in the directory with their validation', (done) => {
-        mdLinks('./test/')
-            .then(result => {
-                expect(result).toEqual([[{
-                    "file": "test\\text.md",
-                    "href": "https://nodejs.org/pt-br/",
-                    "text": "Node js"
-                },
-                {
-                    "file": "test\\text.md",
-                    "href": "https://developers.google.com/v8/",
-                    "text": "motor de JavaScript V8 do Chrome"
-                }],
-                [{
-                    "file": "test\\text.md",
-                    "href": "https://nodejs.org/pt-br/",
-                    "text": "Node js"
-                }, {
-                    "file": "test\\text.md",
-                    "href": "https://developers.google.com/v8/",
-                    "text": "motor de JavaScript V8 do Chrome"
-                }]]
-                )
-                done();
-            })
-    })
-
-
-
-    test('erro', (done) => {
-        mdLinks('./tests/text.md')
-            .catch(err => {
-                expect(err).toBe("Sorry, but there's no archive with that name in this directory");
-                done()
-            })
-    })
+      })
+  })
 })
