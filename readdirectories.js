6f1ac1e5aa2b4a4dbf7baf2624401856b7a6d50b
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 12:17:25 -0300

    função ReadPath refatorada

--- readdirectories.js
+++ readdirectories.js
@@ -1,22 +1,10 @@
 const readdir = require('readdir-recursive');
-const checkLinks = require('./checkhttp.js')
-const mdLinks = require('./index.js')
 
-function readPath(readPath, validation, stats) {
+function readPath(readPath, func) {
     readdir.file(readPath, (file) => {
         if (/\.md/g.test(file) && !/node_modules/g.test(file)) {
-            
-            mdLinks(file)
-                .then((result) => {
-                    const filteredResult = result.filter( item =>/(http[s]?|www)/.test(item.href) )
-                        checkLinks(filteredResult, validation, stats)
-
-                })
-
+            func(file)
         }
-    }
-    )
-
+    })
 }
-
 module.exports = readPath;
