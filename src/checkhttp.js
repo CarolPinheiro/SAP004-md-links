310c73de17d2ac4e94ebaa8e9f9f6bf25dd850cb
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 20:03:27 -0300

    checkhttp.js  corrigido conforme as normas do airbnb

--- src/checkhttp.js
+++ src/checkhttp.js
@@ -1,15 +1,26 @@
-const https = require('https')
+const https = require('https');
+
 function checkIfALinkExist(results, file) {
   return new Promise((resolved) => {
-      https.get(results[2], (res) => {
-        resolved({ text: results[1], href: results[2], file, statusCode: res.statusCode, message: res.statusMessage })
-      })
-        .on('error', (e) => {
-          resolved({ text: results[1], href: results[2], file, statusCode: 404, message: e.message })
-        })
-
-  })
+    https.get(results[2], (res) => {
+      resolved({
+        text: results[1],
+        href: results[2],
+        file,
+        statusCode: res.statusCode,
+        message: res.statusMessage,
+      });
+    })
+      .on('error', (e) => {
+        resolved({
+          text: results[1],
+          href: results[2],
+          file,
+          statusCode: 404,
+          message: e.message,
+        });
+      });
+  });
 }
 
 module.exports = checkIfALinkExist;
-
