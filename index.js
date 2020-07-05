6204f61d09a7caca6368d89bac3cbf4ad94cb2bd
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 13:27:03 -0300

    Md-links refatorada para poder aceitar a leitura de diretórios

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const fs = require('fs');
-const checkIfALinkExist = require('./checkhttp');
 const readPath = require('./readdirectories');
+const readFile = require('./readFileAndExtractData');
 
-function mdLinks(file, options) {
+function mdLinks(file, options=[]) {
   return new Promise((resolved, rejected) => {
-    fs.stat(file, (err, stats) => {
-      if (err) return err
-      else if (stats.isDirectory()) {
-        readPath(file, mdLinks)
+    fs.stat(file, (err, stats)=> {
+      if(err)  rejected(err)
+      else if(stats.isDirectory()) {
+        resolved(readPath(file, options))
       }
-      else if (stats.isFile()) {
-        fs.readFile(file, 'utf-8', (err, data) => {
-          if (err) {
-            rejected(err.message);
-          } else {
-            const regex = data.match(/\[([^\]]*)\]\(([^)]*)/gm);
-            const validate = regex.map(i => {
-              const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-              return checkIfALinkExist(result, file)
-            })
-            const textResult = regex.map((i) => {
-              const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-              return { text: result[1], href: result[2], file, }
-            });
-            options === '--validate' ? resolved(Promise.all(validate)) : resolved(Promise.resolve(textResult))
-
-          }
-        })
+      else if(stats.isFile()) {
+        resolved(readFile(file, options))
       }
     });
   })
