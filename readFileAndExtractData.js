13f75679a218caa70f9243e83e1fd504d5b85d09
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 13:27:23 -0300

    Função ReadFile criada para que a leitura e extração dos links seja feita a parte

---
+++ readFileAndExtractData.js
@@ -0,0 +1,24 @@
+const checkIfALinkExist = require('./checkhttp');
+const fs = require('fs');
+
+function readFile(file, options) {
+    return new Promise((resolved) => {
+        fs.readFile(file, 'utf-8', (err, data) => {
+            if (err) {
+                rejected(err.message);
+            }
+            const regex = data.match(/\[([^\]]*)\]\(([^)]*)/gm);
+            const validate = regex.map(i => {
+                const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+                return checkIfALinkExist(result, file)
+            })
+            const textResult = regex.map((i) => {
+                const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+                return { text: result[1], href: result[2], file }
+            });
+            options.includes('--validate') ? resolved(Promise.all(validate)) : resolved(Promise.resolve(textResult))
+        })
+    })
+}
+
+module.exports = readFile;
