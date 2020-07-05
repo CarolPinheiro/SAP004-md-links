6b51299fc72920e07ddd722ed230d6d514707882
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 12:39:05 -0300

    saída dos elementos no console refatorada

--- cli.js
+++ cli.js
@@ -9,6 +9,16 @@ const validadeOrStats = process.argv[3]
 mdLinks(file, validadeOrStats)
 .then(res => {
     res.map(e=> {
-        console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
+        if(e.statusCode) {
+            console.log(`\n Text:${chalk.cyan.bold(e.text)} 
+            \n Href:${chalk.magenta.bold(e.href)} 
+            \n Path:${chalk.blue.bold(e.file)}
+            \n StatusCode:${e.statusCode>400&&e.statusCode<500?chalk.red.bold(e.statusCode): chalk.green.bold(e.statusCode)}
+            \n Status Message:${chalk.white.bold(e.message)} \n`)
+        }
+        else{
+            console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
+
+        }
     })
 })
