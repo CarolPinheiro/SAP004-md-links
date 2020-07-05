a4d682e7d287df2fb7943750f9ca225944e16db2
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 12:38:35 -0300

    Refatoração dos nomes dos itens no objeto retornado

--- checkhttp.js
+++ checkhttp.js
@@ -4,10 +4,10 @@ function checkIfALinkExist(resultado, file) {
   return new Promise((resolved) => {
 
     https.get(resultado[2], (res) => {
-      resolved({ text: resultado[1], href: resultado[2], path: file, validation: res.statusCode, message: res.statusMessage })
+      resolved({ text: resultado[1], href: resultado[2], file, statusCode: res.statusCode, message: res.statusMessage })
     })
       .on('error', (e) => {
-        resolved({ text: resultado[1], href: resultado[2], path: file, validation: 404, message: e.message })
+        resolved({ text: resultado[1], href: resultado[2], file, statusCode: 404, message: e.message })
       })
 
   })
