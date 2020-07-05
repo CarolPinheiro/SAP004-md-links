eb16e67d6cb8f230d116eb99544c2edb880e3e6c
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 12:15:42 -0300

    Mdlinks em nova versão, agora recebe as opções de validação diretamente dentro dela

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const fs = require('fs');
 
-let arr = [];
-
-function mdLinks(file) {
-  //console.log(file)
+function mdLinks(file, options) {
   return new Promise((resolved, rejected) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
-      if (err) {
-        rejected(err.message);
-      } else {
-        const regex = data.match(/\[([^\]]*)\]\(([^)]*)/gm);
-        const textResult = regex.map((i) => {
-          const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-          return { text: result[1], href: result[2], file }
-        });
-        resolved(textResult);
-        arr = []
+    fs.stat(file, (err, stats) => {
+      if (err) return err
+      else if (stats.isDirectory()) {
+        readPath(file, mdLinks)
+      }
+      else if (stats.isFile()) {
+        fs.readFile(file, 'utf-8', (err, data) => {
+          if (err) {
+            rejected(err.message);
+          } else {
+            const regex = data.match(/\[([^\]]*)\]\(([^)]*)/gm);
+            const validate = regex.map(i => {
+              const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+              return checkIfALinkExist(result, file)
+            })
+            const textResult = regex.map((i) => {
+              const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
+              return { text: result[1], href: result[2], file, }
+            });
+            console.log(textResult)
+            options === '--validate' ? resolved(Promise.all(validate)) : resolved(Promise.resolve(textResult))
+
+          }
+        })
       }
     });
-  });
+  })
 }
-
 module.exports = mdLinks;
