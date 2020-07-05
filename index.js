557edfb7ebcdb6c040cab9f349aeeb55a8d5da6e
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 15:43:23 -0300

    Texto de erro caso o arquivo não seja encontrado alterado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const readFile = require('./readFileAndExtractData');
 function mdLinks(file, options=[]) {
   return new Promise((resolved, rejected) => {
     fs.stat(file, (err, stats)=> {
-      if(err)  rejected(err)
+      if(err)  rejected(`Sorry, but there's no archive with that name in this directory`)
       else if(stats.isDirectory()) {
         resolved(readPath(file, options))
       }
