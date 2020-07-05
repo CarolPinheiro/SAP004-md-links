006595924ed6c18a433d27ece1c68ad5990fe796
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 19:50:40 -0300

    eslintrc corrigido conforme as normas do airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 'off',
+    'prefer-promise-reject-errors': 'off',
+  },
+};
