0271f3a1b2b11dd12e1ee532a6c9ab7dad945f39
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 20:04:06 -0300

    Readdirectories corrigido conforme as normas do airbnb

--- src/readdirectories.js
+++ src/readdirectories.js
@@ -1,18 +1,24 @@
 const readdir = require('readdir-recursive');
+
 const readFile = require('./readFileAndExtractData');
-const arr = []
+
+const arr = [];
 
 function readPath(path, options = []) {
   return new Promise((resolved) => {
     readdir.file(path, (file) => {
       if (/\.md/g.test(file) && !/node_modules/g.test(file)) {
-        arr.push(file)
+        arr.push(file);
         setTimeout(() => {
-          arr.length > 1 ? resolved(Promise.all(arr.map(i => readFile(i, options)))) : resolved(readFile(arr[0], options))
-        }, 3000)
+          if (arr.length > 1) {
+            resolved(Promise.all(arr.map((i) => readFile(i, options))));
+          } else {
+            resolved(readFile(arr[0], options));
+          }
+        }, 3000);
       }
-    })
-  })
+    });
+  });
 }
 
 module.exports = readPath;
