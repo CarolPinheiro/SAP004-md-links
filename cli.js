86e273392f0558eb503b4980d32591589af77965
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 19:50:08 -0300

    Cli corrigido conforme as normas do eslint do airbnb

--- cli.js
+++ cli.js
@@ -1,39 +1,31 @@
 #!/usr/bin/env node
-const mdLinks = require('./index.js');
-const chalk = require('chalk');
 const path = require('path');
-const statsReturn = require('./src/stats')
-
+const chalk = require('chalk');
+const mdLinks = require('./index.js');
+const statsReturn = require('./src/stats');
 
 function checkUsersNeed(file, validadeOrStats) {
-
   mdLinks(file, validadeOrStats)
-    .then(res => {
-      const flatResult = res.flat()
-      const statsReturned =  statsReturn(flatResult)
+    .then((res) => {
+      const flatResult = res.flat();
+      const statsReturned = statsReturn(flatResult);
       if (validadeOrStats.includes('--stats') && validadeOrStats.includes('--validate')) {
-        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}\nBroken:${chalk.red.bold(statsReturned.brokenLinks)}`)
-
+        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}\nBroken:${chalk.red.bold(statsReturned.brokenLinks)}`);
       } else if (validadeOrStats.includes('--stats')) {
-        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}`)
-
+        console.log(`All:${chalk.yellow.bold(statsReturned.allLinks)} \nUnique:${chalk.green.bold(statsReturned.notRepeatedLinks)}`);
       } else {
-        flatResult.map(e => {
+        flatResult.map((e) => {
           if (e.statusCode) {
-            console.log(`\nText:${chalk.cyan.bold(e.text)}\nHref:${chalk.magenta.bold(e.href)}\nPath:${chalk.blue.bold(e.file)}\nStatusCode:${e.statusCode > 400 && e.statusCode < 500 ? chalk.red.bold(e.statusCode) : chalk.green.bold(e.statusCode)}\nStatus Message:${chalk.white.bold(e.message)} \n`)
+            return console.log(`\nText:${chalk.cyan.bold(e.text)}\nHref:${chalk.magenta.bold(e.href)}\nPath:${chalk.blue.bold(e.file)}\nStatusCode:${e.statusCode > 400 && e.statusCode < 500 ? chalk.red.bold(e.statusCode) : chalk.green.bold(e.statusCode)}\nStatus Message:${chalk.white.bold(e.message)} \n`);
           }
-          else {
-            console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
-
-          }
-        })
+          return console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`);
+        });
       }
-    })
+    });
 }
+
 const file = process.argv[2];
-const filePath = path.resolve(file)
-const validadeOrStats = process.argv
+const filePath = path.resolve(file);
+const validadeOrStats = process.argv;
 
-checkUsersNeed(filePath, validadeOrStats)
-
-
+checkUsersNeed(filePath, validadeOrStats);
