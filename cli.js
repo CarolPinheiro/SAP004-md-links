5a9480a51cd8e65b86681daf6dafb9b072c02a3a
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 14:36:01 -0300

    Método flat adicionado, para que o retorno da readPath que pode ter vários arrays encadeados seja tornado em apenas um

--- cli.js
+++ cli.js
@@ -7,18 +7,19 @@ const path = require('path');
 const file = process.argv[2];
 const validadeOrStats = process.argv[3]
 mdLinks(file, validadeOrStats)
-.then(res => {
-    res.map(e=> {
-        if(e.statusCode) {
-            console.log(`\n Text:${chalk.cyan.bold(e.text)} 
-            \n Href:${chalk.magenta.bold(e.href)} 
-            \n Path:${chalk.blue.bold(e.file)}
-            \n StatusCode:${e.statusCode>400&&e.statusCode<500?chalk.red.bold(e.statusCode): chalk.green.bold(e.statusCode)}
-            \n Status Message:${chalk.white.bold(e.message)} \n`)
-        }
-        else{
-            console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
+    .then(res => {
+        const flatResult = res.flat()
+        flatResult.map(e => {
+            if (e.statusCode) {
+                console.log(`\n Text:${chalk.cyan.bold(e.text)} 
+                Href:${chalk.magenta.bold(e.href)} 
+                Path:${chalk.blue.bold(e.file)}
+                StatusCode:${e.statusCode > 400 && e.statusCode < 500 ? chalk.red.bold(e.statusCode) : chalk.green.bold(e.statusCode)}
+                Status Message:${chalk.white.bold(e.message)} \n`)
+            }
+            else {
+                console.log(`\n Text:${chalk.cyan.bold(e.text)} \n Href:${chalk.magenta.bold(e.href)} \n Path:${chalk.blue.bold(e.file)}`)
 
-        }
+            }
+        })
     })
-})
