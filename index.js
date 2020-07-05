1c4906a1be03af103e663a4cfe252fc5265f32f1
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-05 20:02:39 -0300

    Index.js corrigido conforme as normas do airbnb

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const fs = require('fs');
 const readPath = require('./src/readdirectories');
 const readFile = require('./src/readFileAndExtractData');
 
-function mdLinks(file, options=[]) {
+function mdLinks(file, options = []) {
   return new Promise((resolved, rejected) => {
-    fs.stat(file, (err, stats)=> {
-      if(err)  rejected(`Sorry, but there's no archive with that name in this directory`)
-      else if(stats.isDirectory()) {
-        resolved(readPath(file, options))
-      }
-      else if(stats.isFile()) {
-        resolved(readFile(file, options))
+    fs.stat(file, (err, stats) => {
+      if (err) rejected('Sorry, but there is no archive with that name in this directory');
+      else if (stats.isDirectory()) {
+        resolved(readPath(file, options));
+      } else if (stats.isFile()) {
+        resolved(readFile(file, options));
       }
     });
-  })
+  });
 }
 module.exports = mdLinks;
