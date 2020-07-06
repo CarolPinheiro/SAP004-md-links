c9c1f9110e9a038fcebf44e18af9ed465ba82700
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-06 17:12:33 -0300

    Checkhttp agora cobre tanto http quanto https

--- src/checkhttp.js
+++ src/checkhttp.js
@@ -1,25 +1,46 @@
 const https = require('https');
+const http = require('http');
+const path = require('path');
+
+function rejectedReturn(results, e, file) {
+  return {
+    text: results[1],
+    href: results[2],
+    file: path.resolve(file),
+    statusCode: 404,
+    message: e.message,
+  };
+}
+
+function resolvedReturn(results, res, file) {
+  return {
+    text: results[1],
+    href: results[2],
+    file: path.resolve(file),
+    statusCode: res.statusCode,
+    message: res.statusMessage,
+  };
+}
 
 function checkIfALinkExist(results, file) {
-  return new Promise((resolved) => {
-    https.get(results[2], (res) => {
-      resolved({
-        text: results[1],
-        href: results[2],
-        file,
-        statusCode: res.statusCode,
-        message: res.statusMessage,
-      });
-    })
-      .on('error', (e) => {
-        resolved({
-          text: results[1],
-          href: results[2],
-          file,
-          statusCode: 404,
-          message: e.message,
+  return new Promise((resolved, rejected) => {
+    if (/https/.test(results[2])) {
+      https.get(results[2], (res) => {
+        resolved(resolvedReturn(results, res, file));
+      })
+        .on('error', (e) => {
+          resolved(rejectedReturn(results, e, file));
         });
-      });
+    } else if (/http/.test(results[2])) {
+      http.get(results[2], (res) => {
+        resolved(resolvedReturn(results, res, file));
+      })
+        .on('error', (e) => {
+          resolved(rejectedReturn(results, e, file));
+        });
+    } else {
+      rejected('The link is bad formatted');
+    }
   });
 }
 
