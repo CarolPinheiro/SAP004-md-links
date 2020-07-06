9b089fd85502483a5ac7b6e798f6df031061067f
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-06 18:12:31 -0300

    Agora os caminhos são relativos a máquina onde estão sendo testados

--- test/mock.js
+++ test/mock.js
@@ -1,13 +1,15 @@
+const path = require('path');
+
 const arrOfObj = [
   {
     href: 'http://nodejs.org/pt-br/',
     text: 'Node js',
-    file: 'C:\\Users\\ceopi\\Documents\\Laboratoria\\SAP004-md-links\\test\\text.md',
+    file: path.resolve('test', 'text.md'),
   },
   {
     href: 'https://developers.google.com/v8/',
     text: 'motor de JavaScript V8 do Chrome',
-    file: 'C:\\Users\\ceopi\\Documents\\Laboratoria\\SAP004-md-links\\test\\text.md',
+    file: path.resolve('test', 'text.md'),
   },
 ];
 
@@ -15,14 +17,14 @@ const arrOfObjValidated = [
   {
     href: 'http://nodejs.org/pt-br/',
     text: 'Node js',
-    file: 'C:\\Users\\ceopi\\Documents\\Laboratoria\\SAP004-md-links\\test\\text.md',
+    file: path.resolve('test', 'text.md'),
     message: 'Moved Permanently',
     statusCode: 301,
   },
   {
     href: 'https://developers.google.com/v8/',
     text: 'motor de JavaScript V8 do Chrome',
-    file: 'C:\\Users\\ceopi\\Documents\\Laboratoria\\SAP004-md-links\\test\\text.md',
+    file: path.resolve('test', 'text.md'),
     message: 'Moved Permanently',
     statusCode: 301,
   },
