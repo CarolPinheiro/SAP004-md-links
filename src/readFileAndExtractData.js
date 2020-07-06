a6ef65e6c16f7ed40c8bd153ecb4a4da52ed9ef8
Caroline Pinheiro <ommasshi@gmail.com>
2020-07-06 17:25:20 -0300

    Agora o caminho retornado é o absoluto em todos os casos

--- src/readFileAndExtractData.js
+++ src/readFileAndExtractData.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const checkIfALinkExist = require('./checkhttp');
 
 function readFile(file, options) {
@@ -10,11 +11,11 @@ function readFile(file, options) {
       const regex = data.match(/\[([^\]]*)\]\((http[s]?[^)]*)/gm);
       const validate = regex.map((i) => {
         const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-        return checkIfALinkExist(result, file);
+        return checkIfALinkExist(result, path.resolve(file));
       });
       const textResult = regex.map((i) => {
         const result = i.match(/\[([^\]]*)\]\(([^)]*)/);
-        return { text: result[1], href: result[2], file };
+        return { text: result[1], href: result[2], file: path.resolve(file) };
       });
       if (options.includes('--validate')) {
         resolved(Promise.all(validate));
